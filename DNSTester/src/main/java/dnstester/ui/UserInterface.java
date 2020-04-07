adea0bab13813d100930f44c8da3f04340c0158d
Petri Riihikallio <petri.riihikallio@metis.fi>
2020-04-07 22:37:53 +0300

    Button works

--- DNSTester/src/main/java/dnstester/ui/UserInterface.java
+++ DNSTester/src/main/java/dnstester/ui/UserInterface.java
@@ -1,5 +1,7 @@
 package dnstester.ui;
 
+import dnstester.domain.Tester;
+import dnstester.domain.TestResult;
 import javafx.application.Application;
 import javafx.geometry.*;
 import javafx.scene.*;
@@ -11,16 +13,27 @@ public class UserInterface extends Application {
 
     @Override
     public void start(Stage stage) {
+        TestResult result = new TestResult();
         stage.setTitle("DNS Tester");
-        
+
         VBox layout = new VBox(5);
         layout.setPadding(new Insets(10));
         layout.getChildren().add(new Label("DNS Server IP address:"));
-        layout.getChildren().add(new TextField("8.8.8.8"));
+        TextField server = new TextField("172.16.0.9");
+        layout.getChildren().add(server);
         layout.getChildren().add(new Label("Domain name to test with:"));
-        layout.getChildren().add(new TextField("www.example.com"));
-        layout.getChildren().add(new Button("Test"));
-        
+        TextField name = new TextField("www.example.com");
+        layout.getChildren().add(name);
+
+        Button btn = new Button("Test");
+        btn.setOnAction((e) -> {
+            Tester tester = new Tester();
+            if (server.getText().length() > 0 && name.getText().length() > 0) {
+                tester.sendQuery(server.getText(), name.getText());
+            }
+        });
+        layout.getChildren().add(btn);
+
         Scene scene = new Scene(layout);
         stage.setScene(scene);
         stage.show();
