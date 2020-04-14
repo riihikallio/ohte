401ca1861718e778708244930c8a57c3ca0f957d
Petri Riihikallio <petri.riihikallio@metis.fi>
2020-04-14 21:11:32 +0300

    Initial tests

---
+++ DNSTester/src/test/java/dnstester/domain/TesterTest.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dnstester.domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pera
+ */
+public class TesterTest {
+
+    Tester tester;
+
+    public TesterTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        tester = new Tester();
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test(timeout = 1000)
+    public void testerCreated() {
+        assertNotNull("Tester object not created", tester);
+    }
+
+    @Test(timeout = 5000)
+    public void testerSuccess() {
+        TestResult result = tester.sendQuery("8.8.8.8", "www.example.com");
+        assertTrue("Zero time elapsed", result.time > 0);
+        assertFalse("Error flag set", result.fail);
+        assertFalse("Lost flag set", result.lost);
+    }
+
+    @Test(timeout = 1000)
+    public void queryNameMissing() {
+        TestResult result = tester.sendQuery("8.8.8.8", ".");
+        assertTrue("Error flag not set", result.fail);
+        assertEquals("Empty name not detected", "Empty query name", result.error);
+        assertFalse("Lost flag set", result.lost);
+    }
+
+    @Test(timeout = 1000)
+    public void queryNamePartCheck() {
+        TestResult result = tester.sendQuery("8.8.8.8", "www..com");
+        assertTrue("Error flag not set", result.fail);
+        assertEquals("Double dots not detected", "Invalid query name", result.error);
+        assertFalse("Lost flag set", result.lost);
+    }
+
+    @Test(timeout = 1000)
+    public void queryNamePartTooLong() {
+        final String name = "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww.example.com";
+        TestResult result = tester.sendQuery("8.8.8.8", name);
+        assertTrue("Error flag not set", result.fail);
+        assertEquals("Name part exceeding 63 chars not detected", "Invalid query name", result.error);
+        assertFalse("Lost flag set", result.lost);
+    }
+
+    @Test(timeout = 1000)
+    public void queryNameTooLong() {
+        String name = "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww.example.com";
+        name = name + "." + name + "." + name + "." + name + "." + name + "." + name + "." + name;
+        TestResult result = tester.sendQuery("8.8.8.8", name);
+        assertTrue("Error flag not set", result.fail);
+        assertEquals("Name too long not detected", "Test query name too long", result.error);
+        assertFalse("Lost flag set", result.lost);
+    }
+
+    @Test(timeout = 1000)
+    public void queryServerNotFound() {
+        TestResult result = tester.sendQuery("X", "www.example.com");
+        assertTrue("Error flag not set", result.fail);
+        assertTrue("Invalide server address not detected", result.error != null &&
+                result.error.contains("nodename nor servname provided"));
+        assertFalse("Lost flag set", result.lost);
+    }
+
+}
